67f6c166fe978df0de0bcf5b73f9eb43f8262c1d
Manuela Sanchez <m.sanchez947@Hotmail.com>
2021-05-02 10:46:48 -0400

    fixed scroll button

--- components/FirstSection.js
+++ components/FirstSection.js
@@ -1,6 +1,5 @@
 import ScrollButton from './ScrollButton.js'
 import styles from '../styles/Layout.module.css'
-import stylestwo from '../styles/ScrollButton.module.css'
 
 const FirstSection = () => {
   return (
@@ -9,7 +8,7 @@ const FirstSection = () => {
         <h1>Hola! My name is Manuela Sanchez</h1>
         <p>and I am a Junior Software Engineer</p>
       </div>
-      <div className={stylestwo.containingitall}>
+      <div style={{paddingTop:'378px'}}>
         <p>click here and find out who I is</p>
         <ScrollButton></ScrollButton>
       </div>
--- components/ScrollButton.js
+++ components/ScrollButton.js
@@ -5,17 +5,12 @@ import { faArrowDown} from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/ScrollButton.module.css'
 
 const ScrollButton = () => {
-  const arrows = "fa-arrow-down"
   return (
-    <div>
-      <div className={styles.circlecontainer}>
-        <span className={styles.circle}>
-          <div className={`${styles.fa}`}>
-            <FontAwesomeIcon icon={faArrowDown} />
-          </div>
-        </span>
-        <span className={styles.pulse}></span>
-      </div>
+    <div className={styles.circlecontainer}>
+      <span className={styles.circle}>
+        <FontAwesomeIcon  className={`${styles.fa}`} icon={faArrowDown} />
+      </span>
+      <span className={styles.pulse}></span>
     </div>
   )
 }
